e9c832c7f2fbda4e2c821dd36189d484a83b799b
Bo Benjamin Blichfeldt Henriksen <stumpdenmark@hotmail.com>
2014-09-15 13:18:21 +0200

    Latest commit of version 1

--- app/app.js
+++ app/app.js
@@ -6,13 +6,5 @@ angular.module('KSA_Bladr.mocks', ['ngMockE2E']);
 angular.module('KSA_Bladr', [
   'KSA_Bladr.controllers',
   'KSA_Bladr.services',
-  'formly',
-  'autocomplete',
-  'kildeviser-directives',
   'ui.bootstrap'
 ]);
-
-// Main Formly Module
-angular.module('formly', ['formly.render']);
-angular.module('formly.render', []);
-angular.module('kildeviser-directives', []);
