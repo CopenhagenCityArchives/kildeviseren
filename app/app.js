b5de0223f86a92b2f2e58a513401f0d13571dc7d
Bo Benjamin Blichfeldt Henriksen <stumpdenmark@hotmail.com>
2014-09-23 11:06:43 +0200

    Added routing

--- app/app.js
+++ app/app.js
@@ -3,8 +3,35 @@ angular.module('KSA_Bladr.services',[]);
 angular.module('KSA_Bladr.controllers',[]);
 angular.module('KSA_Bladr.mocks', ['ngMockE2E']);
 
-angular.module('KSA_Bladr', [
+var app = angular.module('KSA_Bladr', [
   'KSA_Bladr.controllers',
   'KSA_Bladr.services',
-  'localytics.directives'
+  'localytics.directives',
+  'ngTouch',
+  'ngRoute'
 ]);
+
+// configure our routes
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+    /**
+    Administration
+    **/
+    // route for single archiveversion in admin view
+    .when('/:collectionId', {
+        templateUrl: '/partials-filter.html',
+        controller: 'ViserCtrl'
+    })
+    // route for list of archiveversions in admin view
+    .when('/:collectionId/:itemId', {
+        templateUrl: '/partials-viewer.html',
+        controller: 'ViserCtrl'
+    })
+    // route for list of archiveversions in admin view
+    .when('/', {
+        templateUrl: '/partials-filter.html',
+        controller: 'ViserCtrl'
+    });
+
+    $locationProvider.html5Mode(false);
+});
