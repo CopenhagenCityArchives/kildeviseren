aa512d36fefcbeebaf9fd8b844bae10d4970ac74
Bo Benjamin Blichfeldt Henriksen <stumpdenmark@hotmail.com>
2014-12-18 18:59:53 +0100

    Crawler.php puts more dynamic content on the page

--- app/crawl.php
+++ app/crawl.php
@@ -14,13 +14,15 @@
         public $Levels;
         public $Data;
         public $Output;
+        public $MetadataString;
+        public $SiteURL;
 
         public function KildeViserSEO(){
             error_reporting(E_ALL);
-
-            if(isset($_GET['collection']) && isset($_GET['item'])){
-                $this->collectionId = $_GET['collection'];
-                $this->itemId = $_GET['item'];
+            $input = $this->getURLParameters();
+            if(isset($input['collection']) && isset($input['item'])){
+                $this->collectionId = $input['collection'];
+                $this->itemId = $input['item'];
 
                 $this->collectionUrl = "http://www.kbhkilder.dk/api/collections/" . $this->collectionId;
                 $this->levelsUrl = "http://www.kbhkilder.dk/api/levels/" . $this->collectionId;
@@ -28,7 +30,26 @@
 
                 $this->_fetchJSONData();
                 $this->_buildOutput();
+                $this->_createMetadataString();
+                $this->_createSiteURL();
                 $this->_createTemplate();
+            }
+        }
+
+        private function getURLParameters(){
+            //The only input should be the url string. The loop will only iterate through one entry
+            foreach($_GET as $key => $value){
+                //Expected input: ?collection=3&item=2232
+                $queryString = rawurldecode($key);
+                $inputs = explode('&', $queryString);
+                $inputs[0] = str_replace('?', '', $inputs[0]);
+                $info = array();
+                foreach($inputs as $cur){
+                    $parts = explode('=', $cur);
+                    $info[$parts[0]] = $parts[1];
+                }
+
+                return $info;
             }
         }
 
@@ -38,6 +59,11 @@
             $this->Levels = $this->_getData($this->levelsUrl, $this->Levels);
             $this->Data = $this->_getData($this->dataUrl, $this->Data);
             $this->Data = $this->Data[0];
+
+            //Special treatment of tiled images
+            if($this->Collection['image_type'] != 'image' ){
+                $this->Data['images'][0] = str_replace('_files/', '.jpg', $this->Data['images'][0]);
+            }
         }
 
         private function _buildOutput(){
@@ -45,18 +71,39 @@
             $i = 0;
             //building name:value pairs for data not hidden. If name is hidden it is set to blank
             foreach($this->Levels as $level){
-                if($level['gui_hide_name'] == false){
-                    $temp[$i]['name'] = $level['gui_name'];
-                }
-                else{
-                    $temp[$i]['name'] = false;
-                }
+                if(!$level['gui_hide_value']){
+                    if($level['gui_hide_name'] == false){
+                        $temp[$i]['name'] = $level['gui_name'];
+                    }
+                    else{
+                        $temp[$i]['name'] = false;
+                    }
 
-                $temp[$i]['value'] = $this->Data['metadata'][$level['name']];
+                    $temp[$i]['value'] = $this->Data['metadata'][$level['name']];
+                }
                 $i++;
+
             }
 
             $this->Output = $temp;
+        }
+
+        private function _createMetadataString(){
+            $string = '';
+            foreach($this->Output as $curLevel){
+                $string = $curLevel['name'] ? $string . ' ' . $curLevel['name'] . ': ' . $curLevel['value'] : $string . ' ' . $curLevel['value'];
+            }
+
+            $this->MetadataString = trim($string);
+        }
+
+        private function _createSiteURL(){
+            $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+            $url = rawurldecode($url);
+            $url = str_replace('?_escaped_fragment_=', '#!', $url);
+
+            $this->SiteURL = $url;
         }
 
         private function _getData($url, $data){
@@ -77,6 +124,6 @@
             require($this->templateName);
         }
     }
-    
+
     $new = new KildeViserSEO();
 ?>
