daf65454ecee841bc4bee65a26b69759087b3474
Bo Henriksen <stumpdenmark@hotmail.com>
2017-03-29 13:45:31 +0200

    Changed endpoint urls

--- app/services/URLBuilderService.js
+++ app/services/URLBuilderService.js
@@ -53,8 +53,8 @@ angular.module('KSA_Bladr.services').
         };
 
         //Url of the remote server, base of all requests
-        pubs.remoteServerUrl = "http://www.kbhkilder.dk/api/";
-        //pubs.remoteServerUrl = "http://192.168.10.129/api/";
+        pubs.remoteServerUrl = "http://www.kbhkilder.dk/1508/stable/api/";
+        //pubs.remoteServerUrl = "http://localhost:8000/api/";
 
         pubs.collectionsUrl = function(){
             return pubs.remoteServerUrl + 'collections/' + '?callback=JSON_CALLBACK';
