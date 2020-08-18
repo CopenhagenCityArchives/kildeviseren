7378f70b3af53a105a02c0174ca7490c3efad9f4
Jonsabban <Sanox3@hotmail.dk>
2020-08-18 13:45:21 +0200

    Dynaform: fixed id conflict

--- src/client/directives/dynaform.js
+++ src/client/directives/dynaform.js
@@ -35,7 +35,7 @@ angular.module('KSA_Bladr.directives').directive('dynaform', ['$compile', '$time
                         filterArray.push(value.text);
                     });
 
-                    var elem = angular.element('#filter-' + i);
+                    var elem = angular.element('#filter-' + i + '-container');
 
                     accessibleAutocomplete({
                         element: elem[0],
@@ -47,7 +47,7 @@ angular.module('KSA_Bladr.directives').directive('dynaform', ['$compile', '$time
 
                         tNoResults: () => 'Ingen resultater fundet',
                         tAssistiveHint: () => 'Filtrene kan navigeres med pilekasterne, og enter for at vælge. Filtrene indeholder nuværende værdier der kan slettes for at se alle tilgænglige værdier',
-                        tStatusSelectedOption: (selectedOption, length, index) => '${selectedOption} ${index + 1} af ${length} er highlighted',
+                        tStatusSelectedOption: (selectedOption, length, index) => `${selectedOption} ${index + 1} af ${length} er highlighted`,
                     })
 
                     filterArray = [];
