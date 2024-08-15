9809c759f3a72df43948194825780ec84d801c07
be0z <be0z@GUL-JENS>
2024-08-15 11:19:42 +0200

    Chnaged config values to use staging env

--- profile/kbharkiv-staging/config.js
+++ profile/kbharkiv-staging/config.js
@@ -3,10 +3,10 @@ var config = angular.module('KSA_Bladr.config');
 config.constant('config', {
     errorReporting: true,
     googleAnalyticsTrackingCode: false,
-    redirectUrl: "https://kbharkiv.dk/sog-i-arkivet/kilder-pa-nettet",
-    archiveUrl: "https://kbharkiv.dk",
+    redirectUrl: "https://dev.kbharkiv.dk/sog-i-arkivet/kilder-pa-nettet",
+    archiveUrl: "https://dev.kbharkiv.dk",
     archiveUrlName: "Københavns Stadsarkiv",
-    permalinkUrl: "https://kbharkiv.dk/permalink/source/",
-    apiUrl: "https://api.kbharkiv.dk/"
+    permalinkUrl: "https://dev.kbharkiv.dk/permalink/source/",
+    apiUrl: "https://api-dev.kbharkiv.dk/"
 });
     
